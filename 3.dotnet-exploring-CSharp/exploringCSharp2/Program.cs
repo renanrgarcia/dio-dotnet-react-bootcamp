69c27bdbae93e798d3663e6813088603a1a02b82
Renan <renanrgarcia@gmail.com>
2024-08-16 18:45:16 -0300

    tuples donw

--- 3.dotnet-exploring-CSharp/exploringCSharp2/Program.cs
+++ 3.dotnet-exploring-CSharp/exploringCSharp2/Program.cs
@@ -1,2 +1,21 @@
-﻿(int, string, string, decimal) tuple = (1, "Renan", "Garcia", 1000.00m);
+﻿using exploringCSharp2.Models;
+
+// (int, string, string, decimal) tuple = (1, "Renan", "Garcia", 1000.00m);
+
+FileRead fileRead = new FileRead();
+
+var (success, fileLines, lineCount) = fileRead.ReadFile("Files/fileToRead.txt");
+
+if (success)
+{
+  foreach (var line in fileLines)
+  {
+    Console.WriteLine(line);
+  }
+  Console.WriteLine($"There are {lineCount} lines in the file");
+}
+else
+{
+  Console.WriteLine("Something went wrong");
+}
 
