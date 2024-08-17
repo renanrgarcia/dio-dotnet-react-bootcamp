c8f0df138c151813963491485280d644721fd932
Renan Garcia <renanrgarcia@gmail.com>
2024-08-17 08:36:10 -0300

    3.dotnet-exploring-CSharp - Serializing Json

--- 3.dotnet-exploring-CSharp/jsonProject/Program.cs
+++ 3.dotnet-exploring-CSharp/jsonProject/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using jsonProject.Models;
+using Newtonsoft.Json;
+
+List<Sale> sales = new List<Sale>();
+
+Sale s1 = new Sale(1, "Notebook", 2500.00m, DateTime.Now);
+Sale s2 = new Sale(2, "Tablet", 1500.00m, DateTime.Now);
+Sale s3 = new Sale(3, "Smartphone", 800.00m, DateTime.Now);
+
+sales.Add(s1);
+sales.Add(s2);
+sales.Add(s3);
+
+// JSON serialization
+string serializedJson = JsonConvert.SerializeObject(sales, Formatting.Indented);
+
+File.WriteAllText("Files/sales.json", serializedJson);
+
+Console.WriteLine(serializedJson);
