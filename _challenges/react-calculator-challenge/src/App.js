de5145da051b11d7d8938cedfa97798227b9b2f5
Renan <renanrgarcia@gmail.com>
2024-09-05 18:03:35 -0300

    Adding react to a project

---
+++ _challenges/react-calculator-challenge/src/App.js
@@ -0,0 +1,13 @@
+import { Container, Content } from "./styles";
+
+const App = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>Hello World</h1>
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ _challenges/react-calculator-challenge/src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: #CACACA;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #FFFFFF;
+    width: 80%;
+    min-height: 350px;
+`
