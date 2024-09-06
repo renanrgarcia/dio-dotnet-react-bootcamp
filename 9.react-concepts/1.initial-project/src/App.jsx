63c35b3bcc967ef304a80eea941f77c8e7593574
Renan <renanrgarcia@gmail.com>
2024-09-06 17:24:36 -0300

    Important concepts

--- 9.react-concepts/1.initial-project/src/App.jsx
+++ 9.react-concepts/1.initial-project/src/App.jsx
@@ -1,7 +1,42 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
+    const [user, setUser] = useState("");
+    const [count, setCount] = useState(0);
+    const [users, setUsers] = useState(
+        ["Renan", "Thaís"]
+    );
+    
+    const handleUser = () => {
+        setUsers([...users, user]);
+        setUser("");
+    }
+
+    useEffect(() => {
+        setCount(users.length);
+    }, [users]);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setUsers([...users, "Aurora", "Pedro"]);
+    //     }, 3000);
+    // }, []);
+
     return (
         <div className="App">
             <h1>Hello React</h1>
+            <h3>Count: {count}</h3>
+            <div>
+                <input 
+                    value={user}
+                    onChange={(event) => setUser(event.target.value)}
+                />
+                <button onClick={handleUser}>Add</button>
+            </div>
+
+            {users.map((user) => (
+                <p>{user}</p>
+            ))}
         </div>
     );
 };
