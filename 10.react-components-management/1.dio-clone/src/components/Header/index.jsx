35e632838b0563161f0a265bdafb49ae57c38093
Renan Garcia <renanrgarcia@gmail.com>
2024-09-17 20:40:20 -0300

    Dio clone done

--- 10.react-components-management/1.dio-clone/src/components/Header/index.jsx
+++ 10.react-components-management/1.dio-clone/src/components/Header/index.jsx
@@ -4,8 +4,11 @@ import logo from '../../assets/logo-dio.png';
 import { Button } from '../Button';
 
 import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({autenticado}) => {
+  const navigate = useNavigate()
+
   return (
     <Wrapper>
       <Container>
@@ -27,8 +30,8 @@ const Header = ({autenticado}) => {
               ) : (
               <>
                 <MenuRight href="/">Home</MenuRight>
-                <Button title="Entrar" />
-                <Button title="Cadastrar" />
+                <Button title="Entrar" onClick={() => navigate('/login')} />
+                <Button title="Cadastrar" onClick={() => navigate('/register')} />
               </>)}
           </Row>
       </Container>
